fdf02697c6ea2c7f9d9b419e0f2e52d04708d034
Fiorella Urbina <fiorella.urbina@novatechautomation.com>
2025-09-04 16:59:14 -0500

    fix: dialog now shows on top of other windows

--- bfrbye/dialog.py
+++ bfrbye/dialog.py
@@ -2,8 +2,11 @@ import tkinter as tk
 from tkinter import simpledialog
 
 def show_input_dialog(response):
-    newWin = tk.Tk() 
-    newWin.withdraw() 
+    newWin = tk.Tk()
+    newWin.attributes('-alpha', 0)  # make the window invisible, withdraw doesn't allow to show on top
+    newWin.lift()
+    newWin.attributes('-topmost',True)
+    
     res = simpledialog.askstring("Hands up!", "Tell me why",parent=newWin)
     response.append(res)
     newWin.destroy() 
